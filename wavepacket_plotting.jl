a94eb4b68ad241f5dd07f19e2f60f0a6dcbe0e9c
Tim_Mer <timmymerriman@gmail.com>
2019-02-17 18:03:40 +0000

    adding working equatin code in wavepacket_plotting.jl

--- wavepacket_plotting.jl
+++ wavepacket_plotting.jl
@@ -1,23 +1,8 @@
-x = [0:0.0005:1]
-x_0 = 0.4
-C = 25
-sigma_squared = 1e-3
+x = collect(0:0.0005:1)                         # creates an array from 0 to 1 in steps of 0.0005
+x_0 = fill(0.4, 2001)                           # sets all the following values to their specified values
+C = fill(25.0, 2001)                            # have to use fill so that it can multiply arrays properly
+sigma_squared = fill(1e-3, 2001)
 delta_x = 0.0005
 delta_t = 0.2
-k_0 = 500
-psi = C*exp(-(x-x_0).^2/sigma_squared).*exp(1i*k_0*x)
-# subplot(2,2,1)
-# plot(x,real(psi), 'b')
-# title('Real part of wavefunction ')
-# xlabel('distance')
-# ylabel('Re(wavefunction)')
-# subplot(2,2,2)
-# plot(x,imag(psi),'r')
-# title('Imaginary part of wavefunction')
-# xlabel('distance')
-# ylabel('Im(wavefunction)')
-# subplot(2,2,3)
-# plot(x,(conj(psi).*psi),'k')
-# title('Probability of finding particle  ')
-# xlabel('distance')
-# ylabel('psi*conj(psi)')
+k_0 = 500.0                                     # decimal point so that it creates a float
+psi = C.*exp.((-(x-x_0).^2)./sigma_squared).*exp.((k_0*x)*1im) # equation as in book
