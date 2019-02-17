9ced675f72de437031e5c0d89f638138bcb22aa5
Tim_Mer <timmymerriman@gmail.com>
2019-02-17 13:37:43 +0000

    g

--- wavepacket_plotting.jl
+++ wavepacket_plotting.jl
@@ -1,10 +1,11 @@
-x=(0:0.0005:1)
-x_0=0.4
-C=25
-sigma_squared=1e-3
-delta_x=0.0005
-delta_t=0.2; k_0=500
-psi=C*exp(-(x-x_0).^2/sigma_squared).*exp(1i*k_0*x)
+x = [0:0.0005:1]
+x_0 = 0.4
+C = 25
+sigma_squared = 1e-3
+delta_x = 0.0005
+delta_t = 0.2
+k_0 = 500
+psi = C*exp(-(x-x_0).^2/sigma_squared).*exp(1i*k_0*x)
 # subplot(2,2,1)
 # plot(x,real(psi), 'b')
 # title('Real part of wavefunction ')
