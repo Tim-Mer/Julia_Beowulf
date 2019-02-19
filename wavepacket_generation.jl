1419d24bc370122be1bda1179820a2754aa31d7f
Tim_Mer <timmymerriman@gmail.com>
2019-02-19 17:25:40 +0000

    k

--- wavepacket_generation.jl
+++ wavepacket_generation.jl
@@ -6,11 +6,7 @@ delta_x = 0.0005
 delta_t = 0.2
 k_0 = 500.0                                     # decimal point so that it creates a float
 psi = C.*exp.((-(x-x_0).^2)./sigma_squared).*exp.((k_0*x)*1im) # equation as in book
-using Plots
-plotlyjs()
-subplot(2,2,1)
-plotly.newplot(rand(5,5))
-subplot(2,2,2)
-plotly.newplot(rand(5,5))
-subplot(2,2,3)
-plotly.newplot(rand(5,5))
+<<<<<<< HEAD
+=======
+writedlm("test.txt", psi, ',')
+>>>>>>> 1c62080d237e71e73a107c0245aa9a4e2905b6b9
--- wavepacket_plotting.jl
+++ wavepacket_plotting.jl
@@ -1,8 +1,12 @@
 using Plots
-plotlyjs()
-subplot(2,2,1)
-plotly.newplot(rand(5,5))
-subplot(2,2,2)
-plotly.newplot(rand(5,5))
-subplot(2,2,3)
-plotly.newplot(rand(5,5))
+rpsi = readdlm("test.txt", ',', ComplexF64)
+plotlyJS(rand(5))
+
+#using Plots
+#plotlyjs()
+#subplot(2,2,1)
+#plotly.newplot(rand(5,5))
+#subplot(2,2,2)
+#plotly.newplot(rand(5,5))
+#subplot(2,2,3)
+#plotly.newplot(rand(5,5))
