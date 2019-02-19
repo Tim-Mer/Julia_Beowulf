9894aa2aba3f01dbd11b81430c288f97be10a67c
Tim_Mer <timmymerriman@gmail.com>
2019-02-19 22:18:14 +0000

    updates

--- Scripts/wavepacket_generation.jl
+++ Scripts/wavepacket_generation.jl
@@ -5,7 +5,7 @@ sigma_squared = fill(1e-3, 2001)
 delta_x = 0.0005
 delta_t = 0.2
 k_0 = 500.0                                     # decimal point so that a float is created
-
+# psi or ψ is the wavefunction
 psi = C.*exp.((-(x-x_0).^2)./sigma_squared).*exp.((k_0*x)*1im) # equation as in book
 # using the . is for elementwise operations
 # saving psi and x to files so that they can be read by the plotting script
