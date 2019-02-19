178b610da37e49266c099d5fd995c713b953f2c8
Tim_Mer <timmymerriman@gmail.com>
2019-02-19 18:06:10 +0000

    b

--- wavepacket_generation.jl
+++ wavepacket_generation.jl
@@ -6,11 +6,5 @@ delta_x = 0.0005
 delta_t = 0.2
 k_0 = 500.0                                     # decimal point so that it creates a float
 psi = C.*exp.((-(x-x_0).^2)./sigma_squared).*exp.((k_0*x)*1im) # equation as in book
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
+
 writedlm("test.txt", psi, ',')
->>>>>>> 1c62080d237e71e73a107c0245aa9a4e2905b6b9
-=======
-writedlm("test.txt", psi, ',')
->>>>>>> 1c62080d237e71e73a107c0245aa9a4e2905b6b9
