2cdae1c06e36227d9626565c6db1d611b366b4d8
Tim_Mer <timmymerriman@gmail.com>
2019-02-19 22:03:06 +0000

    adding comments

--- Scripts/wavepacket_generation.jl
+++ Scripts/wavepacket_generation.jl
@@ -4,8 +4,11 @@ C = fill(25.0, 2001)                            # have to use fill so that it ca
 sigma_squared = fill(1e-3, 2001)
 delta_x = 0.0005
 delta_t = 0.2
-k_0 = 500.0                                     # decimal point so that it creates a float
+k_0 = 500.0                                     # decimal point so that a float is created
+
 psi = C.*exp.((-(x-x_0).^2)./sigma_squared).*exp.((k_0*x)*1im) # equation as in book
+# using the . is for elementwise operations
+# saving psi and x to files so that they can be read by the plotting script
 using DelimitedFiles
 writedlm("Files/x.txt", x, ',')
-writedlm("Files/psi.txt", psi, ',')   # writing psi to a file
+writedlm("Files/psi.txt", psi, ',')
--- Scripts/wavepacket_plotting.jl
+++ Scripts/wavepacket_plotting.jl
@@ -1,33 +1,33 @@
+# reading in files using the approatiate datatype
 using DelimitedFiles
 rpsi = readdlm("Files/psi.txt", ',', ComplexF64)
 rx = readdlm("Files/x.txt", ',', Float64)
 
+# plotting all 3 plots
 using Plots
-#plt = plot(layout = 3)
 p1 =    plot(rx,real(rpsi),
         title = "Real part of wavefunction",
         xlabel = "Distance",
         ylabel = "Re(wavefunction)",
-        #subplot = 1,
         legend = false
         )
 p2 =    plot(rx,imag(rpsi),
         title = "Imaginary part of wavefunction",
         xlabel = "Distance",
         ylabel = "Im(wavefunction)",
-        #subplot = 2,
         legend = false
         )
 p3 =    plot(rx,real(rpsi.*conj(rpsi)),
         title = "Probability of finding a particle",
         xlabel = "Distance",
         ylabel = "psi*conj(psi)",
-        #subplot = 3,
         legend = false
         )
-plt = plot(p1, p2, p3, layout = 3)
+plt = plot(p1, p2, p3, layout = 3) # plotting all 3 plots on one page
+# saving the plots as .png
 savefig(p1, "Figures/Plot1.png")
 savefig(p2, "Figures/Plot2.png")
 savefig(p3, "Figures/Plot3.png")
 savefig(plt, "Figures/Plots.png")
+# displays the plt in atom
 display(plt)
