ecb70c2008349efe92cc0a368ef01b95419e1d64
Tim-Mer <timmymerriman@gmail.com>
2019-02-19 18:03:55 +0000

    asdf

--- wavepacket_plotting.jl
+++ wavepacket_plotting.jl
@@ -1,12 +1,11 @@
 using Plots
 rpsi = readdlm("test.txt", ',', ComplexF64)
-plotlyJS(rand(5))
 
 #using Plots
 #plotlyjs()
 #subplot(2,2,1)
 #plotly.newplot(rand(5,5))
 #subplot(2,2,2)
-#plotly.newplot(rand(5,5)) 
+#plotly.newplot(rand(5,5))
 #subplot(2,2,3)
 #plotly.newplot(rand(5,5))
