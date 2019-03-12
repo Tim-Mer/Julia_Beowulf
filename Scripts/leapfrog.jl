8fc18c928e9b0c50eae530dd6b948df31d6ef6d5
Tim_Mer <timmymerriman@gmail.com>
2019-03-12 19:59:58 +0000

    up

--- Scripts/leapfrog.jl
+++ Scripts/leapfrog.jl
@@ -7,52 +7,32 @@ x_0 = fill(0.4, N)
 C = fill(10.0, N)
 sigma_squared = fill(1e-3, N)
 k_0 = 500.0
-<<<<<<< HEAD
 Δ_x = 1e-3
 Δ_t = 5e-8
 ψ = C.*exp.((-(x-x_0).^2)./σ_sqrd).*exp.((k_0*x)*1im)
 R_cur = real(ψ)
 I_cur = imag(ψ)
-=======
-delta_x = 1e-3
-delta_t = 5e-8
-psi = C.*exp.((-(x-x_0).^2)./sigma_squared).*exp.((k_0*x)*1im)
-R_cur = real(psi)
-I_cur = imag(psi)
->>>>>>> 2dd72c1befc1f7e20dd676091f2b05b73cdf9225
 V = fill(0.0, N)
 for i = 600:N
    V[i] = -1e6
 end
-<<<<<<< HEAD
 I_next = imag_psi(N, I_cur, R_cur, Δ_t, Δ_x, V)
 
 # Do the leapfrog
 anim = @animate for time_step = 1:15000
    global R_cur, I_cur
    R_next = real_psi(N, R_cur, I_cur, Δ_t, Δ_x, V)
-=======
-I_next = imag_psi(N, I_cur, R_cur, delta_t, delta_x, V)
-# Do the leapfrog
-anim = @animate for time_step in 1:15000
-   global R_cur, I_cur
-   R_next = real_psi(N, R_cur, I_cur, delta_t, delta_x, V)
->>>>>>> 2dd72c1befc1f7e20dd676091f2b05b73cdf9225
    R_cur = R_next
-   I_next = imag_psi(N, I_cur, R_cur, delta_t, delta_x, V)
+   I_next = imag_psi(N, I_cur, R_cur, Δ_t, Δ_x, V)
    prob_density = R_cur.^2+I_next.*I_cur
    I_cur = I_next
-   plot(x, prob_density,#'-b','LineWidth',2
+   plot(x, prob_density,
    title = "Reflection from cliff",
-   # axis([0 1 0 200])
    xlabel = "x",
    ylabel = "Probability density",
+   ylims(0,1),
    legend = false
    )
 end every 10
 
-<<<<<<< HEAD
 gif(anim, "./Figures/LeapFrog.gif", fps=30)
-=======
-gif(anim, "Figures/LeapFrog.gif", fps=30)
->>>>>>> 2dd72c1befc1f7e20dd676091f2b05b73cdf9225
--- Scripts/shooting.jl
+++ Scripts/shooting.jl
@@ -1,43 +1,39 @@
-clear
-N=200
-delta_x=0.01
-E_initial=1.879
-delta_E=0.1
-x=(delta_x: delta_x: N*delta_x)
-V= zeros(1, N)
-V(100:N) =1000
-b= 1.5
-last_diverge=0
-  minimum_delta_E=0.005
-  while abs(delta_E)>minimum_delta_E
-    psi= zeros(1, N)
-    psi(1)=1
-    psi(2)=1
-    [psi,i]=calculate_psi(psi, N, delta_x, E, b,V)
-    plot(x, psi,'r')
-    title('Square well')
-    axis([0 2 -2 2])
-    xlabel('distance')
-    ylabel('Wavefunction')
-    drawnow
-    pause(0.5)
-    if sign(psi(i+1))~=sign(last_diverge)
-    delta_E=-delta_E/2;
-  end
-  E=E+delta_E
-  last_diverge=sign(psi(i+1))
+using Plots
+include("calculate.jl")
+N = 200
+delta_x = 0.01
+E_initial = 1.879
+delta_E = 0.1
+x = collect(delta_x: delta_x: N*delta_x)
+V = fill(0.0, N);
+for i = 100:N
+  V[i] = 1000
 end
+b = 1.5
+last_diverge = 0
+minimum_delta_E = 0.005
+E = E_initial
 
 
 
-# goes in seperate file
-function [psi,i] = calculate_psi(psi, N, delta_x, E, b,V)
-#This function calculates psi
-# Make psi_prime(0) =0 for an even parity solution;
-  for i=2:N-1;
-  psi(i+1)=2*psi(i)-psi(i-1)-2*(E-V(i))*delta_x^2*psi(i)
-  if abs(psi(i+1)) > b
-  #  if psi is diverging, exit the loop
-  return
+while abs(delta_E) > minimum_delta_E
+    global E, psi, i
+    psi = fill(0.0, N)
+    psi[1] = 1
+    psi[2] = 1
+    psi,i = calculate_psi(psi, N, delta_x, E, b,V)
+    anim = plot(x, psi,
+    title = "Square well",
+    xlims = (0, 2),
+    ylims = (-2, 2),
+    xlabel = "distance",
+    ylabel = "Wavefunction",
+    legend = false
+    )
+    frame(anim)
+    if sign(psi[i+1])~=sign(last_diverge)
+        delta_E=-delta_E/2;
+    end
+    E=E+delta_E;
+    last_diverge=sign(psi[i+1]);
   end
-end
