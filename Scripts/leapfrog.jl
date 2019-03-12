d07368abf00d78a81fa8d7d23b3e2d066e072549
Tim_Mer <timmymerriman@gmail.com>
2019-03-12 19:35:41 +0000

    p

--- Scripts/leapfrog.jl
+++ Scripts/leapfrog.jl
@@ -10,19 +10,17 @@ k_0 = 500.0
 Δ_x = 1e-3
 Δ_t = 5e-8
 ψ = C.*exp.((-(x-x_0).^2)./σ_sqrd).*exp.((k_0*x)*1im)
-R_init = real(psi)
-I_init = imag(psi)
+R_cur = real(ψ)
+I_cur = imag(ψ)
 V = fill(0.0, N)
-for i in 600:N
+for i = 600:N
    V[i] = -1e6
 end
-I_cur = I_init
-R_cur = R_init
-# t=t+delta_t/2;
 I_next = imag_psi(N, I_cur, R_cur, Δ_t, Δ_x, V)
+
 # Do the leapfrog
-anim = @animate for time_step in 1:15000
-   global R_cur, I_cur#, R_next, I_next, N, Δ_t, Δ_x, V
+anim = @animate for time_step = 1:15000
+   global R_cur, I_cur
    R_next = real_psi(N, R_cur, I_cur, Δ_t, Δ_x, V)
    R_cur = R_next
    I_next = imag_psi(N, I_cur, R_cur, Δ_t, Δ_x, V)
@@ -36,10 +34,5 @@ anim = @animate for time_step in 1:15000
       legend = false
    )
 end every 10
-<<<<<<< HEAD
-using Plots
-gif(anim, "Figures/LeapFrog.gif")
-=======
 
 gif(anim, "./Figures/LeapFrog.gif", fps=30)
->>>>>>> d786c45ab42d253561134c79a58716e39163dd6b
