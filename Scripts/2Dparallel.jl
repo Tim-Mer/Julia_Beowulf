90238b51b20edb4a2198b5ed7821db5b8fe9026c
Tim_Mer <timmymerriman@gmail.com>
2019-04-30 19:36:33 +0100

    move all gather

--- Scripts/2Dparallel.jl
+++ Scripts/2Dparallel.jl
@@ -16,9 +16,9 @@ function imag_psi_2D(N, I_current, R_current, delta_t, delta_x, V, comm)
             y = y+2
          end
          I_next[x,y]=I_current[x,y] +s*(R_current[x+1,y]-2*R_current[x,y]+R_current[x-1,y]+R_current[x,y+1]-2*R_current[x,y]+R_current[x,y-1])-delta_t*V[x,y].*R_current[x,y]
-         MPI.Allgather(I_next, comm)
       end
    end
+   MPI.Allgather(I_next, comm)
    MPI.Barrier(comm)
    return I_next
 end
@@ -38,9 +38,9 @@ function real_psi_2D(N, R_current, I_current, delta_t, delta_x, V, comm)
             y = y+2
          end
          R_next[x,y] = R_current[x,y] - s*(I_current[x+1,y]-2*I_current[x,y]+I_current[x-1,y]+I_current[x,y+1]-2*I_current[x,y]+I_current[x,y-1])+delta_t*V[x,y].*I_current[x,y]
-         MPI.Allgather(R_next, comm)
       end
    end
+   MPI.Allgather(R_next, comm)
    MPI.Barrier(comm)
    return R_next
 end
