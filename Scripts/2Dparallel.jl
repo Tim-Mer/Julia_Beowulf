d2577e6fc89f6f8f9953952e22c22a68323241f9
Tim_Mer <timmymerriman@gmail.com>
2019-04-30 19:11:36 +0100

    try same again

--- Scripts/2Dparallel.jl
+++ Scripts/2Dparallel.jl
@@ -98,6 +98,7 @@ function main()
    println("Hello world, I am $(MPI.Comm_rank(comm)) of $(MPI.Comm_size(comm))")
    MPI.Barrier(comm)
    anim = @animate for time_step = 1:20
+      local prob_density
       if MPI.Comm_rank(comm) == 0
          println("Time Step: ", time_step)
       end
@@ -110,7 +111,6 @@ function main()
       I_next = imag_psi_2D(N, I_current, R_current, delta_t, delta_x, V, comm)
       MPI.Barrier(comm)
       if MPI.Comm_rank(comm) == 0
-         local prob_density
          prob_density = R_current.^2 + I_next.*I_current
          I_current = I_next
       end
