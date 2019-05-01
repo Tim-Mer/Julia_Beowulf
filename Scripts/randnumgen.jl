81d8910fdc6fcc9c56b06e9a7e14c0e119d208eb
Tim_Mer <timmymerriman@gmail.com>
2019-05-01 22:51:22 +0100

    moving file correctly

--- Scripts/randnumgen.jl
+++ Scripts/randnumgen.jl
@@ -5,20 +5,30 @@ function randnum(r)
     return rand(r, Float64, 1)
 end
 
+function random()
+    r = MT19937()
+    length = 0
+        while length < 40
+            f = open("./Files/randnum.txt", "w")
+            write(f, "$(randnum(r)) \n")
+            if(MPI.Comm_rank(comm) == 0)
+                length+=MPI.Comm_size(comm)
+            end
+            MPI.Barrier(comm)
+        end
+    end
+end
+
 function main()
     MPI.Init()
     comm = MPI.COMM_WORLD
-    r = MT19937()
-    length = 0
-    open("./Files/randnum.txt", "w") do f
-        while length < 40
-            write(f, "$(randnum(r)) \n")
-            MPI.Barrier(comm)
-            length+=1
-        end
+    if(MPI.Comm_rank(comm) == 0)
+        touch("./Files/randnum.txt")
+        rm("./Files/randnum.txt")
     end
+    MPI.Barrier(comm)
+
     MPI.Finalize()
 end
-touch("./Files/randnum.txt")
-rm("./Files/randnum.txt")
-@time main()
+
+main()
