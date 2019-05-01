203194289519d8f657355f4ffa432f1390874621
Tim_Mer <timmymerriman@gmail.com>
2019-05-01 22:59:19 +0100

    adding N

--- Scripts/randnumgen.jl
+++ Scripts/randnumgen.jl
@@ -5,13 +5,14 @@ function randnum(r)
     return rand(r, Float64, 1)
 end
 
-function random(comm)
+function random(N, comm)
     r = MT19937()
     length = 0
     f = open("./Files/randnum.txt", "w")
-        while length < 40
+        while length < N
             write(f, "$(randnum(r)) \n")
             if(MPI.Comm_rank(comm) == 0)
+                println(N)
                 length+=MPI.Comm_size(comm)
             end
             MPI.Barrier(comm)
@@ -22,12 +23,14 @@ end
 function main()
     MPI.Init()
     comm = MPI.COMM_WORLD
+    N = 50
     if(MPI.Comm_rank(comm) == 0)
         touch("./Files/randnum.txt")
         rm("./Files/randnum.txt")
+        println("Starting random number generation of $N random numbers!")
     end
     MPI.Barrier(comm)
-    random(comm)
+    random(N, comm)
 
     MPI.Finalize()
 end
