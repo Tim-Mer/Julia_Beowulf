c0b21c78a84a12af160640c644942f7127b6c290
Tim_Mer <timmymerriman@gmail.com>
2019-05-01 23:46:29 +0100

    chnging N again

--- Scripts/randnumgen.jl
+++ Scripts/randnumgen.jl
@@ -26,7 +26,7 @@ end
 function main()
     MPI.Init()
     comm = MPI.COMM_WORLD
-    N = 200000#000
+    N = 200000000
     if(MPI.Comm_rank(comm) == 0)
         #touch("./Files/randnum.txt")
         #rm("./Files/randnum.txt")
