2b864b6ea3374eb8df1538a9f88247aac3a67adc
Tim_Mer <timmymerriman@gmail.com>
2019-05-01 22:31:14 +0100

    adding mpi

--- paralleltest/randnumgen.jl
+++ paralleltest/randnumgen.jl
@@ -1,17 +1,24 @@
 using RandomNumbers.MersenneTwisters
+using MPI
 
 function randnum(r)
     return rand(r, Float64, 1)
 end
 
 function main()
+    MPI.Init()
+    comm = MPI.COMM_WORLD
     r = MT19937()
+    length = 0
     open("./Files/randnum.txt", "w") do f
-        for i = 1:10000000
+        while length < 40
             write(f, "$(randnum(r)) \n")
+            MPI.Barrier(comm)
+            length+=1
         end
     end
+    MPI.Finalize()
 end
-
+touch("./Files/randnum.txt")
 rm("./Files/randnum.txt")
 @time main()
