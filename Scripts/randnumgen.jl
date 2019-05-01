73eaebd86f424bf953f33b2ec37972d789bc5e0f
Tim_Mer <timmymerriman@gmail.com>
2019-05-01 23:26:02 +0100

    not using files

--- Scripts/randnumgen.jl
+++ Scripts/randnumgen.jl
@@ -8,22 +8,23 @@ end
 function random(N, comm)
     r = MT19937()
     length = 1
-    f = open("./Files/randnum.txt", "w")
+    #f = open("./Files/randnum.txt", "w")
         while length < N
             MPI.Barrier(comm)
-            write(f, "$(randnum(r)) \n")
+            x = rand(r, Float64, N)
+            #write(f, "$(randnum(r)) \n")
             length+=1
         end
-    close(f)
+    #close(f)
 end
 
 function main()
     MPI.Init()
     comm = MPI.COMM_WORLD
-    N = 100000
+    N = 400000000
     if(MPI.Comm_rank(comm) == 0)
-        touch("./Files/randnum.txt")
-        rm("./Files/randnum.txt")
+        #touch("./Files/randnum.txt")
+        #rm("./Files/randnum.txt")
         println("Starting random number generation of $N random numbers!")
     end
     MPI.Barrier(comm)
