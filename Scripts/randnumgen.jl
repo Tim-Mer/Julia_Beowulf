9972113d5fc8807c19ff2168b4df4132cf69501d
Tim_Mer <timmymerriman@gmail.com>
2019-05-02 21:30:32 +0100

    test

--- Scripts/randnumgen.jl
+++ Scripts/randnumgen.jl
@@ -43,16 +43,21 @@ dest = 0
 nb_elms = 2
 no_assert = 0
 #function test()
-for i = 0:(convert(Int64, N/size)-1)
-    A = zeros(0)
-    for j = 1:(convert(Int64, N/size))
-        append!(A, randnum(r))
-    end
-    MPI.Win_lock(MPI.LOCK_EXCLUSIVE, dest, no_assert, win)
-    MPI.Put(A, length(A), dest, offset, win)
-    MPI.Win_unlock(dest, win)
+A = zeros(0)
+if rank == dest
+    println("Length of A before: ", length(A))
 end
-    MPI.Barrier(comm)
+MPI.Barrier(comm)
+for j = 1:(convert(Int64, N/size))
+    append!(A, randnum(r))
+end
+MPI.Win_lock(MPI.LOCK_EXCLUSIVE, dest, no_assert, win)
+MPI.Put(A, length(A), dest, offset, win)
+MPI.Win_unlock(dest, win)
+if rank == dest
+    println("Length of A after: ", length(A))
+end
+MPI.Barrier(comm)
 #end
 #@time test()
 MPI.Barrier(comm)
