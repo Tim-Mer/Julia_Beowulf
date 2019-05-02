9d6657553bc64e937dc478966bcd35d964fbe70d
Tim_Mer <timmymerriman@gmail.com>
2019-05-02 22:01:38 +0100

    timing from with main

--- Scripts/simplerng.jl
+++ Scripts/simplerng.jl
@@ -5,18 +5,22 @@ function randnum(r)
     return rand(r, Float64, 1)
 end
 
-MPI.Init()
-const comm = MPI.COMM_WORLD
-const rank = MPI.Comm_rank(comm)
-const size = MPI.Comm_size(comm)
-N = convert(Int64, 24*100000000)
-r = MT19937()
-if(rank == 0)
-    println("Starting random number generation of $N random numbers!")
+function main(n)
+    MPI.Init()
+    comm = MPI.COMM_WORLD
+    rank = MPI.Comm_rank(comm)
+    size = MPI.Comm_size(comm)
+    N = convert(Int64, 24*n)
+    r = MT19937()
+    if(rank == 0)
+        println("Starting random number generation of $N random numbers!")
+    end
+    MPI.Barrier(comm)
+    for j = 1:(convert(Int64, N/size))
+        randnum(r)
+    end
+    MPI.Barrier(comm)
+    MPI.Finalize()
 end
-MPI.Barrier(comm)
-for j = 1:(convert(Int64, N/size))
-    randnum(r)
-end
-MPI.Barrier(comm)
-MPI.Finalize()
+n = 100000000
+@time main(n)
