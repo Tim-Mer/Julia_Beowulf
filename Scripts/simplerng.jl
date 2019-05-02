9b162f4b6296b72d7cdc1c20326ee6e3a971f5d9
Tim_Mer <timmymerriman@gmail.com>
2019-05-02 21:58:12 +0100

    adding simple rng file

---
+++ Scripts/simplerng.jl
@@ -0,0 +1,22 @@
+using RandomNumbers.MersenneTwisters
+using MPI
+
+function randnum(r)
+    return rand(r, Float64, 1)
+end
+
+MPI.Init()
+const comm = MPI.COMM_WORLD
+const rank = MPI.Comm_rank(comm)
+const size = MPI.Comm_size(comm)
+N = convert(Int64, 24*4000000)
+r = MT19937()
+if(rank == 0)
+    println("Starting random number generation of $N random numbers!")
+end
+MPI.Barrier(comm)
+for j = 1:(convert(Int64, N/size))
+    randnum(r)
+end
+MPI.Barrier(comm)
+MPI.Finalize()
