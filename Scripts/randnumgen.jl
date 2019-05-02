a156fd3d0287a32fd5cbfe9ac140bb4f8c64c228
Tim_Mer <timmymerriman@gmail.com>
2019-05-02 20:08:37 +0100

    testing MPI

--- Scripts/randnumgen.jl
+++ Scripts/randnumgen.jl
@@ -1,41 +1,54 @@
 using RandomNumbers.MersenneTwisters
 using MPI
+import Printf: @printf
 
 function randnum(r, n)
     return rand(r, Float64, n)
 end
 
+function speed_test(r, N) where {T<:Number}
+    A = Array{T}(undef, N)
+    rand!(r, A)
+    elapsed = @elapsed rand!(r, A)
+    elapsed * N / N * 8 / sizeof(T)
+end
+
 function random(N, comm)
     r = MT19937()
-    x = zeros(N)
-    n = convert(Int64, N/MPI.Comm_size(comm))
-    MPI.Scatter(x, n, 0, comm)
-    len = 1
-    #f = open("./Files/randnum.txt", "w")
-    while len < n
-        MPI.Barrier(comm)
-        randnum(r, 1)
-        #write(f, "$(randnum(r)) \n")
-        len+=1
-    end
-    if(MPI.Comm_rank(comm) == 0)
-        println("Length x: $(length(x))")
-    end
-    MPI.Barrier(comm)
-    #close(f)
+    fo = open("$(MPI.Comm_size(comm)).log")
+    redirect_stdout(fo)
+    speed = speed_test(r, N)
+    @printf "Speed Test: %.3f ns/64 bits\n" speed
+    flush(fo)
+    close(fo)
 end
 
 function main()
     MPI.Init()
     comm = MPI.COMM_WORLD
-    N = 2000000#00
+    rank = MPI.Comm_rank(comm)
+    N = 200#000000
     if(MPI.Comm_rank(comm) == 0)
-        #touch("./Files/randnum.txt")
-        #rm("./Files/randnum.txt")
+        touch("./Files/randnum.log")
+        rm("./Files/randnum.log")
         println("Starting random number generation of $N random numbers!")
     end
+    shared = zeros(N)
+    win = MPI.Win()
+    MPI.Win_create(shared, MPI.INFO_NULL, comm, win)
     MPI.Barrier(comm)
-    random(N, comm)
+    offset = rank
+    dest = 0
+    nb_elms = 1
+    MPI.Win_lock(MPI.LOCK_EXCLUSIVE,dest, no_assert, win)
+    MPI.Put([Float64(rank)], nb_elms, dest, offset, win)
+    MPI.Win_unlock(dest, win)
+    if rank == dest
+        MPI.Win_lock(MPI.LOCK_SHARED, dest, no_assert, win)
+        println("I was sent this: ", shared')
+        MPI.Win_unlock(dest, win)
+    end
+    #random(N, comm)
 
     MPI.Finalize()
 end
