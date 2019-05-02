a57e89ff3a72906695e661d604a28e8b4197a968
Tim_Mer <timmymerriman@gmail.com>
2019-05-02 21:34:54 +0100

    less numbers

--- Scripts/randnumgen.jl
+++ Scripts/randnumgen.jl
@@ -10,7 +10,7 @@ MPI.Init()
 const comm = MPI.COMM_WORLD
 const rank = MPI.Comm_rank(comm)
 const size = MPI.Comm_size(comm)
-N = convert(Int64, 24*100000000)
+N = convert(Int64, 24*1000000)
 r = MT19937()
 if(MPI.Comm_rank(comm) == 0)
     touch("./Files/randnum.log")
