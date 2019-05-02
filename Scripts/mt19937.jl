5d4cb2d3440443d3a7a27288375f6f0b1cc58866
Tim_Mer <timmymerriman@gmail.com>
2019-05-02 19:14:03 +0100

    adding const

--- Scripts/mt19937.jl
+++ Scripts/mt19937.jl
@@ -1,10 +1,10 @@
 include("common.jl")
 using RandomNumbers.MersenneTwisters
+const TEST_NAME = "Beowulf_Cluster"
 #using MPI
 function main()
     #MPI.Init()
     #comm = MPI.COMM_WORLD
-    TEST_NAME = "Beowulf_Cluster"
     r = MT19937()
     test_all(r, 10000)
     #MPI.Finalize()
