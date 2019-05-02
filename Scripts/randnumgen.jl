9a640db95b362fb6e82d06ce127b47595f83096d
Tim_Mer <timmymerriman@gmail.com>
2019-05-02 20:20:24 +0100

    change elms

--- Scripts/randnumgen.jl
+++ Scripts/randnumgen.jl
@@ -38,7 +38,7 @@ MPI.Win_create(shared, MPI.INFO_NULL, comm, win)
 MPI.Barrier(comm)
 offset = rank
 dest = 0
-nb_elms = 1
+nb_elms = 2
 no_assert = 0
 MPI.Win_lock(MPI.LOCK_EXCLUSIVE, dest, no_assert, win)
 MPI.Put([Float64(rank)], nb_elms, dest, offset, win)
