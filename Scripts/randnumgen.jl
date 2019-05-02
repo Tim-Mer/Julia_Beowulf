48ef046dbbb0ad18595d679d2236b36eb3e273bd
Tim_Mer <timmymerriman@gmail.com>
2019-05-02 21:55:06 +0100

    just numbers

--- Scripts/randnumgen.jl
+++ Scripts/randnumgen.jl
@@ -13,26 +13,26 @@ const size = MPI.Comm_size(comm)
 N = convert(Int64, 24*4000000)
 r = MT19937()
 if(MPI.Comm_rank(comm) == 0)
-    touch("./Files/randnum.log")
-    rm("./Files/randnum.log")
+#    touch("./Files/randnum.log")
+#    rm("./Files/randnum.log")
     println("Starting random number generation of $N random numbers!")
 end
-shared = zeros(N)
-win = MPI.Win()
-MPI.Win_create(shared, MPI.INFO_NULL, comm, win)
-MPI.Barrier(comm)
-offset = convert(Int64, N*(rank/size))
+#shared = zeros(N)
+#win = MPI.Win()
+#MPI.Win_create(shared, MPI.INFO_NULL, comm, win)
+#MPI.Barrier(comm)
+#offset = convert(Int64, N*(rank/size))
 dest = 0
-nb_elms = 2
-no_assert = 0
+#nb_elms = 2
+#no_assert = 0
 #function test()
-A = zeros(0)
-if rank == dest
-    println("Length of A before: ", length(A))
-end
+#A = zeros(0)
+#if rank == dest
+#    println("Length of A before: ", length(A))
+#end
 MPI.Barrier(comm)
 for j = 1:(convert(Int64, N/size))
-    append!(A, randnum(r))
+    randnum(r)#append!(A, randnum(r))
 end
 #MPI.Win_lock(MPI.LOCK_EXCLUSIVE, dest, no_assert, win)
 #MPI.Put(A, length(A), dest, offset, win)
@@ -41,7 +41,7 @@ end
     #println("Length of A after: ", length(A))
     #println("Lenght of shared: ", length(shared))
 #end
-MPI.Barrier(comm)
+#MPI.Barrier(comm)
 #end
 #@time test()
 MPI.Barrier(comm)
