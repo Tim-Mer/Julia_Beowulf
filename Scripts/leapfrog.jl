9b583dc099208f9b96360c56d7440d323ef7e7fa
Tim_Mer <timmymerriman@gmail.com>
2019-05-21 22:06:40 +0100

    more report stuff

--- Scripts/leapfrog.jl
+++ Scripts/leapfrog.jl
@@ -5,7 +5,6 @@ using DelimitedFiles
 include("Real.jl")
 include("Imag.jl")
 #function leapfrog()
-#for p = 0:50:600
    ENV["PLOTS_TEST"] = "true" #used to remove errors when running on headless linux
    ENV["GKSwstype"] = "100"
    N = 1000 #resolution 1000
@@ -23,24 +22,24 @@ include("Imag.jl")
    I_cur = imag(ψ)
    V = fill(0.0, N)
    #t = collect(0:1/(pi*50.65):2*pi)
-   #wave = 5000000 .+ 5000000 .* cos.(2 * pi .+ t)
+   #wave = 5000000 .+ 5000000  .* cos.(2 * pi .+ t)
    #length(wave)
    #for i = 1:N
    #   V[i] = -500*i
    #end
    for i = 600:650
-      V[i] = 5e4#1e6#-(i-599)*2500
+      V[i] = -5e5#1e6#-(i-599)*2500
    end
-   for i = 750:800
-      V[i] = 5e4#1e6
-   end
+   #for i = 700:750
+   #   V[i] = 5e5#1e6
+   #end
    #V = wave
    I_next = imag_psi(N, I_cur, R_cur, Δ_t, Δ_x, V)
    #before = fill(0.0, 386)
    #after = before
    # Do the leapfrog
    global R_cur, I_cur, prob_density, before, after
-   anim = @animate for time_step = 1:40000
+   anim = @animate for time_step = 1:20000
       global R_cur, I_cur, prob_density, before, after
       R_next = real_psi(N, R_cur, I_cur, Δ_t, Δ_x, V)
       R_cur = R_next
@@ -54,32 +53,32 @@ include("Imag.jl")
 #         after = filter(!isnan, prob_density[200:585])
 #      end
       plt = plot(x, prob_density,
-         title = "Double barrier k=$(convert(Int64, k_0)) frame $(time_step)",#sigma_sqrd=$(σ_sqrd[1])",#k=$(convert(Int64, k_0)) frame=$(time_step)",
+         title = "Double Barrier k=$(convert(Int64, k_0)) frame $(time_step)",#sigma_sqrd=$(σ_sqrd[1])",#k=$(convert(Int64, k_0)) frame=$(time_step)",
          xlabel = "x",
          ylabel = "Probability density",
          ylims = (0,200),
          xlims = (0,1),
          legend = false,
-         #right_margin = 10mm,
+         right_margin = 10mm,
          show = false
          )
       plt = plot!(twinx(), V,
          xaxis = false,
          grid = false,
          ylims = (0,maximum(V)),
-         xlims = (1, 1000),
+         xlims = (1, N),
          legend = false,
          show = false,
          color = :red
          )
          if time_step == 1 || time_step%2500 == 0
-            savefig(plt, "./Figures/report/test double barrier k=$(convert(Int64, k_0)) frame=$(time_step).png")
+            savefig(plt, "./Figures/report/test Double Barrier V=$(maximum(V)) k=$(convert(Int64, k_0)) frame=$(time_step).png")
             println(time_step)
          end
          #display(plt)
 end every 20
    #percentage = round(100*(((mean(before)-mean(after))/mean(before))); digits=2)
-   gif(anim, "./Figures/test/Leapfrog_double_barrier_test_k=$(convert(Int64, k_0)).gif", fps=30)
+   gif(anim, "./Figures/test/Leapfrog_Double Barrier_test_V=$(maximum(V))_k=$(convert(Int64, k_0)).gif", fps=30)
    #println(percentage)
 #   open("./Files/test.csv", "w") do f
 #      writedlm(f, [before after], ",")
